db73bec379ef16dbca77fafbcf7cf0cae52ddfd0
Sany <dong.dsy0@gmail.com>
2024-11-14 20:11:10 +0800

    test 4

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "我的第一篇博客",
+        "excerpt":"这是我的第一篇博客  ","categories": ["blog"],
+        "tags": ["example","hello"],
+        "url": "/blog/2024/11/14/test1.html",
+        "teaser": null
+      }]
